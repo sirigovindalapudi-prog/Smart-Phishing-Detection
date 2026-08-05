51bda828d20121807c39ad67c37998059ff30ab5
Sirisha <sirigovindalapudi@gmail.com>
2026-08-05 17:22:07 +0530

    Update app.py for Render deployment

--- app.py
+++ app.py
@@ -322,6 +322,8 @@ def logout():
 # Run Application
 # ------------------------------------
 
-if __name__ == "__main__":
+import os
 
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
